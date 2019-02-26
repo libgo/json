07b3c1b3140212744f76b9c4897359f8bfb882cc
elvizlai <sdrzlyz@gmail.com>
2019-02-26 10:02:48 +0800

    first commit

---
+++ json.go
@@ -0,0 +1,56 @@
+package json
+
+import (
+	"bytes"
+
+	jsoniter "github.com/json-iterator/go"
+)
+
+var json = jsoniter.ConfigCompatibleWithStandardLibrary
+
+// func alias, add if needed.
+var (
+	Marshal             = json.Marshal
+	MarshalIndent       = json.MarshalIndent
+	MarshalToString     = json.MarshalToString
+	Unmarshal           = json.Unmarshal
+	UnmarshalFromString = json.UnmarshalFromString
+	NewEncoder          = json.NewEncoder
+	NewDecoder          = json.NewDecoder
+	Valid               = json.Valid
+	Get                 = json.Get
+)
+
+// MustMarshal
+func MustMarshal(v interface{}) []byte {
+	data, _ := Marshal(v)
+	return data
+}
+
+// ToMap convert struct/bytes/string to map[string]interface{}
+func ToMap(v interface{}) map[string]interface{} {
+	if vm, ok := v.(map[string]interface{}); ok {
+		return vm
+	}
+
+	buf := &bytes.Buffer{}
+	switch t := v.(type) {
+	case []byte:
+		buf.Write(t)
+	case string:
+		buf.WriteString(t)
+	default:
+		err := NewEncoder(buf).Encode(v)
+		if err != nil {
+			return nil
+		}
+	}
+
+	var result map[string]interface{}
+	err := NewDecoder(buf).Decode(&result)
+	if err != nil {
+		return nil
+	}
+
+	return result
+}
